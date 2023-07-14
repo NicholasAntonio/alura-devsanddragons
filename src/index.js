f86667f0148fc01928383ce9028d69481e136bd6
“NicholasAntonio” <nicholas.nkt.antonio@gmail.com>
2023-07-14 14:35:03 -0300

    final commit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import { PersonagemView } from "./components/personagem-view.js"
 import { Mago } from "./modules/mago.js"
 import { Arqueiro } from "./modules/arqueiro.js"
 import { ArqueiroMago } from "./modules/arqueiro-mago.js"
+import { Guerreiro } from "./modules/guerreiro.js"
 
 
 const magoAntonio = new Mago('Antonio', 4, 'fogo', 4, 3)
 const magaJulia = new Mago('Júlia', 8, 'gelo', 7, 10)
 const arqueiroJoao = new Arqueiro('João', 6, 6)
 const arqueiroMagoMatheus = new ArqueiroMago('Matheus', 7, 10, 'ar', 4, 8)
+const guerreiroJeferson = new Guerreiro('Jeferson', 8)
 
-const personagens = [magoAntonio, magaJulia, arqueiroJoao, arqueiroMagoMatheus]
+const personagens = [magoAntonio, magaJulia, arqueiroJoao, arqueiroMagoMatheus, guerreiroJeferson]
 
 new PersonagemView(personagens).render()
